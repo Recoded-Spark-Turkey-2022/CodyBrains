b7a020e6134358463f1f9ec5352c088bd6a16ae7
yusuf <yusufpirejman@gmail.com>
2022-12-20 15:01:05 +0300

    bugfix: fix the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import logo from './assets/logo.svg';
-
-function App() {
-  return (
-    <div className="flex items-center gap-2 text-refubookBlue hover:text-refubookRed font-roboto p-4">
-      <img src={logo} alt="" className="" />
-      <h2 className="text-refubookBlue font-semibold"> Refubook</h2>
-=======
-import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 
@@ -18,7 +8,6 @@ function App() {
     <div className="App">
       <Navbar />
       APPJS
->>>>>>> Stashed changes
     </div>
     
   );
