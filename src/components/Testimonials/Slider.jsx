9b9ecf0b0c9ea8bea3cb394f5fd697d2bab8ae44
Yasin Can <canyasin1996@gmail.com>
2022-12-24 01:14:22 +0300

    testimonials and partnets and tests

--- src/components/Testimonials/Slider.jsx
+++ src/components/Testimonials/Slider.jsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
-import featuresData from '../../data/featuresData';
-import star from '../../assets/star.svg';
+import testimonialsData from '../../data/testimonialsData';
 
 // The Slider component is a pagination component that uses SwiperJS to display slides of content.
 const Slider = () => {
@@ -14,23 +13,26 @@ const Slider = () => {
         clickable: true,
       }}
       modules={[Pagination]}
+      className="flex items-center justify-center w-full gap-3"
+      loop
     >
-      {/* We map over the featuresData array to render a SwiperSlide for each item in the array. */}
-      {featuresData.map((item) => {
+      {/* We map over the testimonialsData array to render a SwiperSlide for each item in the array. */}
+      {testimonialsData.map((item) => {
         return (
           // The SwiperSlide component is used to wrap the content that we want to display as a slide.
           // We pass a unique key prop to each SwiperSlide to help with rendering performance.
-          <SwiperSlide key={item.id}>
+          <SwiperSlide key={item.id} className=" flex flex-col  gap-3 ">
             {/* The content for each slide is a div containing an image, a title, and some text. */}
-            <div className=" flex flex-col items-center justify-center gap-5 py-5">
-              <img src={star} alt="" />
-              <p className="text-md  text-refubookLightBlack font-medium text-center">
-                {item.title}
-              </p>
-              <p className="text-md  text-refubookLightBlack font-medium text-center mb-4">
-                {item.text}
-              </p>
-            </div>
+
+            <h3 className="text-lg text-refubookBlack font-normal md:text-justify  text-center">
+              {item.comment}
+            </h3>
+            <span className="text-md  text-refubookLightBlue font-light md:text-left text-center">
+              {item.name}
+            </span>
+            <span className="text-md  text-refubookLightBlue font-light md:text-left text-center mb-8">
+              {item.country}
+            </span>
           </SwiperSlide>
         );
       })}
---
+++ src/data/partnersData.js
@@ -0,0 +1,53 @@
+// This file contains the data for the companies that will be displayed on the home page
+// The data is an array of objects, each object contains the id and the image of the company
+
+import recoded from '../assets/companies/recoded.png';
+import spark from '../assets/companies/spark.png';
+import flatiron from '../assets/companies/flatiron.png';
+import children from '../assets/companies/children.png';
+import unhcr from '../assets/companies/unhcr.png';
+import amnesty from '../assets/companies/amnesty.png';
+import oxfam from '../assets/companies/oxfam.png';
+import giz from '../assets/companies/giz.png';
+import unicef from '../assets/companies/unicef.png';
+
+const partners = [
+  {
+    id: 1,
+    image: recoded,
+  },
+  {
+    id: 2,
+    image: spark,
+  },
+  {
+    id: 3,
+    image: flatiron,
+  },
+  {
+    id: 4,
+    image: children,
+  },
+  {
+    id: 5,
+    image: unhcr,
+  },
+  {
+    id: 6,
+    image: amnesty,
+  },
+  {
+    id: 8,
+    image: oxfam,
+  },
+  {
+    id: 9,
+    image: giz,
+  },
+  {
+    id: 10,
+    image: unicef,
+  },
+];
+
+export default partners;
