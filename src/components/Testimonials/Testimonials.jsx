7bf8dc65558f0b658ef6acca5fc1f38d71fbe4b3
Yasin Can <canyasin1996@gmail.com>
2022-12-24 01:28:03 +0300

    testimonials test and snapshot added

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,4 +1,6 @@
-import Slider from './Slider';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import testimonialsData from '../../data/testimonialsData';
 import map from '../../assets/world-map.svg';
 
 const Testimonials = () => {
@@ -8,7 +10,35 @@ const Testimonials = () => {
         <img src={map} alt="" className="w-96 h-96" />
       </div>
       <div className="flex flex-col items-center justify-center max-w-xs md:max-w-md px-4">
-        <Slider />
+        <Swiper
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="flex items-center justify-center w-full gap-3"
+          loop
+        >
+          {/* We map over the testimonialsData array to render a SwiperSlide for each item in the array. */}
+          {testimonialsData.map((item) => {
+            return (
+              // The SwiperSlide component is used to wrap the content that we want to display as a slide.
+              // We pass a unique key prop to each SwiperSlide to help with rendering performance.
+              <SwiperSlide key={item.id} className=" flex flex-col  gap-3 ">
+                {/* The content for each slide is a div containing an image, a title, and some text. */}
+
+                <h3 className="text-lg text-refubookBlack font-normal md:text-justify  text-center">
+                  {item.comment}
+                </h3>
+                <span className="text-md  text-refubookLightBlue font-light md:text-left text-center">
+                  {item.name}
+                </span>
+                <span className="text-md  text-refubookLightBlue font-light md:text-left text-center mb-8">
+                  {item.country}
+                </span>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
