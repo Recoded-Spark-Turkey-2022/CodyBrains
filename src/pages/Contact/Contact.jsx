7b0e3b6bd1644b804607551a6c65996829c52d76
sara sallat <sallatsara@gmail.com>
2022-12-26 01:02:59 +0300

    changes on contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,31 +5,16 @@ const Contact = () => {
 return (
     <div className="flex flex-col items-center w-full pb-10">
       <h1 className="text-center mt-10 text-7xl text-highlight">
-        Contact Us
+        Get in Touch 
       </h1>
-      <p className="text-center mt-12 text-primary">
-        We are for you ...
-      </p>
-      <p className="text-center text-primary"> Please send us your note here </p>
+      
+      <p className="text-center text-primary"> But Brooke Chaffin and Connors are looking to change that with the introduction   </p>
       <form action="#" className="flex flex-col items-center w-4/6">
         <div className="flex flex-col justify-between w-full mt-10 lg:flex-row">
-          <div className="flex flex-col">
-            <label
-              htmlFor="FullName"
-              className="pl-1.5 text-primary text-xl mb-1"
-            >
-             Enter your full name
-            </label>
-            <input
-              className="border border-primary placeholder-primary rounded-2xl h-11 w-full lg:w-56 bg-secondary placeholder-opacity-100 pl-1.5 box-border outline-none"
-              type="text"
-              id="FullName"
-              placeholder="FullName"
-            />
-          </div>
+          
           <div className="flex flex-col">
             <label htmlFor="email" className="pl-1.5 text-primary text-xl mb-1">
-              Enter your Email
+               Email
             </label>
             <input
               className="border border-primary placeholder-primary rounded-2xl h-11 w-full lg:w-56 bg-secondary placeholder-opacity-100 pl-1.5 box-border outline-none"
@@ -38,29 +23,16 @@ return (
               placeholder="email"
             />
           </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor="phoneNumber"
-              className="pl-1.5 text-primary text-xl mb-1"
-            >
-              Enter your phonr number
-            </label>
-            <input
-              className="border border-primary placeholder-primary rounded-2xl h-11 w-full lg:w-56 bg-secondary placeholder-opacity-100 pl-1.5 box-border outline-none"
-              type="tel"
-              id="phoneNumber"
-              placeholder="phoneNumber"
-            />
-          </div>
+         
         </div>
         <textarea
           className="border border-primary placeholder-primary mt-5 rounded-2xl resize-none bg-secondary placeholder-opacity-100 pl-1.5 pt-1 box-border outline-none w-full"
           name="message"
           rows="10"
-          placeholder="your message"
+          placeholder="Message"
         />
         <button
-          className="mt-5 rounded-2xl w-28 h-10 text-primary bg-highlight active:bg-primary active:text-highlight font-medium"
+          className="mt-5  border-primary rounded-2xl w-28 h-10 text-primary bg-highlight active:bg-primary active:text-highlight font-medium"
           type="submit"
         >
           Send
