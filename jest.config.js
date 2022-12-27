09a0325a9720f3ba8025791a50482a7909d23f95
Helin <helintoptanci3@outlook.com>
2022-12-27 23:51:59 +0300

    added test

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'jsdom',
-  transformIgnorePatterns: ['node_modules/(?!(swiper)/)'],
+  transformIgnorePatterns: ['/node_modules/(?!(swiper|ssr-window|dom7))'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
+  },
+  moduleNameMapper: {
+    'swiper/css': 'swiper/swiper.min.css',
   },
 };
