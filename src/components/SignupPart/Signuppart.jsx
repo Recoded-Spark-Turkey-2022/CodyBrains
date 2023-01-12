28146f0a8ab30d3d5d5e2b1271875ba50cb0cc54
Helin <helintoptanci3@outlook.com>
2023-01-12 22:06:08 +0300

    mobile view change

--- src/components/SignupPart/Signuppart.jsx
+++ src/components/SignupPart/Signuppart.jsx
@@ -5,16 +5,14 @@ import hug from '../../assets/hug.png';
 const SignUp = () => {
   return (
     <section className="flex flex-col">
-      <div className="px-20 mx-20 pb-20 grid place-items-center">
+      <div className=" mx-20 pb-20 grid place-items-center">
         <img className="w-60" src={hug} alt="" />
         <h2 className="text-2xl xl:text-4xl text-refubookBlue mb-4 text-center">
           Share your story, help others and get help from others
         </h2>
         <p className="text-center pt-7 pb-5 px-15 mx-15 text-refubookGray font-medium  text-l max-w-screen">
-          “As a refugee myself, I know the challenges and struggles refugees
-          face. Despite the challenges, I look back on my journey to a new
-          country with gratitude and joy. I am grateful for the opportunities I
-          received in my new country with the help of the wonderful community
+          “I am grateful for the opportunities I
+          received in a new country with the help of the wonderful community
           here.” – Nura, 32
         </p>
 
