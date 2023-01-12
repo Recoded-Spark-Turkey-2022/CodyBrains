b28190dc4a54ee82832b046ccfb1d286b41a18d9
Yasin Can <canyasin1996@gmail.com>
2023-01-12 20:02:30 +0300

    mini changes

--- src/pages/Contact/__test__/Contact.test.jsx
+++ src/pages/Contact/__test__/Contact.test.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Contact from '../Contact';
 
-
-jest.mock();
-
 describe('Contact', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Contact />).toJSON();
