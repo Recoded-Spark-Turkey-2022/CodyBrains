0a8207ca5bb941db67546b9a5f4d4133c0fa8d9d
yusuf <yusufpirejman@gmail.com>
2023-01-12 16:36:16 +0300

    eslint bug about the console log

--- src/components/DropdownComponent/DropdownComponent.jsx
+++ src/components/DropdownComponent/DropdownComponent.jsx
@@ -31,7 +31,7 @@ export default function DropdownComponent() {
   const { t } = useTranslation();
 
   useEffect(() => {
-    console.log('Setting page stuff');
+    // console.log('Setting page stuff');
     document.body.dir = currentLanguage.dir || 'ltr';
     document.title = t('app_title');
   }, [currentLanguage, t]);
