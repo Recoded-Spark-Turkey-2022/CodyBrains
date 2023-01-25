7e26ba7c16a33843cf815558a3b38b7d900a4786
yusuf <yusufpirejman@gmail.com>
2023-01-25 16:49:29 +0300

    testimonials

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,39 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
-import testimonialsData from '../../data/testimonialsData';
+import { useTranslation } from 'react-i18next';
+// import testimonialsData from '../../data/testimonialsData';
 import map from '../../assets/world-map.svg';
 
 const Testimonials = () => {
+  const { t } = useTranslation();
+
+  const testimonialsData = [
+    {
+      id: 1,
+      name: t('Maria'),
+      comment: t('Testimonials_Maria_Comment'),
+      country: t('Mexico'),
+    },
+    {
+      id: 2,
+      name: 'Hassan',
+      comment: t('Testimonials_Hassan_Comment'),
+      country: t('Afghanistan'),
+    },
+    {
+      id: 3,
+      name: t('Olga'),
+      comment: t('Testimonials_Olga_Comment'),
+      country: t('Ukraine'),
+    },
+    {
+      id: 4,
+      name: t('John'),
+      comment: t('Testimonials_John_Comment'),
+      country: t('South Sudan'),
+    },
+  ];
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-evenly w-full bg-refubookWhite px-8 py-4">
       <div className="flex flex-col items-center justify-center max-w-md">
@@ -27,13 +57,15 @@ const Testimonials = () => {
                 {/* The content for each slide is a div containing an image, a title, and some text. */}
 
                 <h3 className="text-lg text-refubookBlack font-normal md:text-justify  text-center">
-                  {item.comment}
+                {t(item.comment)}
+                  
+                  
                 </h3>
                 <span className="text-md  text-refubookLightBlue font-light md:text-left text-center">
-                  {item.name}
+                  {t(item.name)}
                 </span>
                 <span className="text-md  text-refubookLightBlue font-light md:text-left text-center mb-8">
-                  {item.country}
+                  {t(item.country)}
                 </span>
               </SwiperSlide>
             );
